7af58a7e47e7a068bca8d4ff898d86c2463a08c0
marvin <marnyanjui@gmail.com>
2022-03-20 11:48:34 +0300

    Add Toast

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -3,6 +3,9 @@ package com.moringaschool.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Cast View to Button
+        Button findRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        //Add Click Listener to button
+        //Set OnClick takes new onClickListener
+        findRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                //Add Toast
+                //Simple pop-up message when button is clicked
+                //toast(context,message,duration).show();
+                Toast.makeText(MainActivity.this,"Hello There",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
