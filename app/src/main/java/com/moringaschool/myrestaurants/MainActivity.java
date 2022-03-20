6fccdc84097555e3a64b99e5a49b06fd954ec727
marvin <marnyanjui@gmail.com>
2022-03-20 14:41:11 +0300

    Collect User Input

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Refers to the class's simple name :: Same as public static final String TAG = "MainActivity"
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //EditText
+        //Find Edit Text Field
+        EditText locationEditText = (EditText) findViewById(R.id.locationEditText);
 
         //Cast View to Button
         Button findRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
@@ -29,10 +37,15 @@ public class MainActivity extends AppCompatActivity {
                 // toast(context,message,duration).show();
                 // Toast.makeText(MainActivity.this,"Hello There",Toast.LENGTH_LONG).show();
 
+                //Save user input and convert it from editable to a String
+                String location = locationEditText.getText().toString();
+                Log.d(TAG, location);
+
                 //Intent - Provides RunTime Binding between separate components ex: different Activities
                 //params :: packageContext
                 Intent intent = new Intent(MainActivity.this,RestaurantsActivity.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this,location,Toast.LENGTH_LONG).show();
             }
         });
     }
